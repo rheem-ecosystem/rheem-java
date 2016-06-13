87b2c36ac9d12f642492f303588e2a34192af8da
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-06-13 17:32:22 +0200

    feat(rheem-spark): add SparkGlobalMaterializedGroupOperator
    * Spark implementation of GlobalMaterializedGroupOperator
    * provide mapping to this basic Operator
    * add tests

--- src/main/java/org/qcri/rheem/java/mapping/GlobalMaterializedGroupToJavaGlobalMaterializedGroupMapping.java
+++ src/main/java/org/qcri/rheem/java/mapping/GlobalMaterializedGroupToJavaGlobalMaterializedGroupMapping.java
@@ -1,18 +1,16 @@
 package org.qcri.rheem.java.mapping;
 
 import org.qcri.rheem.basic.operators.GlobalMaterializedGroupOperator;
-import org.qcri.rheem.basic.operators.GlobalReduceOperator;
 import org.qcri.rheem.core.mapping.*;
 import org.qcri.rheem.core.types.DataSetType;
 import org.qcri.rheem.java.JavaPlatform;
 import org.qcri.rheem.java.operators.JavaGlobalMaterializedGroupOperator;
-import org.qcri.rheem.java.operators.JavaGlobalReduceOperator;
 
 import java.util.Collection;
 import java.util.Collections;
 
 /**
- * Mapping from {@link GlobalReduceOperator} to {@link JavaGlobalReduceOperator}.
+ * Mapping from {@link GlobalMaterializedGroupOperator} to {@link JavaGlobalMaterializedGroupOperator}.
  */
 @SuppressWarnings("unchecked")
 public class GlobalMaterializedGroupToJavaGlobalMaterializedGroupMapping implements Mapping {
@@ -30,7 +28,7 @@ public class GlobalMaterializedGroupToJavaGlobalMaterializedGroupMapping impleme
 
     private SubplanPattern createSubplanPattern() {
         final OperatorPattern operatorPattern = new OperatorPattern(
-                "group", new GlobalMaterializedGroupOperator<>(null, DataSetType.none()), false);
+                "group", new GlobalMaterializedGroupOperator<>(DataSetType.none(), DataSetType.none()), false);
         return SubplanPattern.createSingleton(operatorPattern);
     }
 
